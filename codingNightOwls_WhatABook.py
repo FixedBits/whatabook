c73e9185e363ac0d47d6fe4d19041782e97e1590
Victor Soto <135081103+FixedBits@users.noreply.github.com>
2024-03-10 08:32:02 -0600

    Update codingNightOwls_WhatABook.py

--- codingNightOwls_WhatABook.py
+++ codingNightOwls_WhatABook.py
@@ -5,42 +5,50 @@ Date: 03/10/2024
 Description: Python App for WhatABook project
 """
 
-# import the MongoClient 
+# Import the MongoClient 
 from pymongo import MongoClient
 
-# connection to MongoDB 
-client = MongoClient("mongodb+srv://web340_admin:webstudent999@bellevueuniversity.bj68fz9.mongodb.net/?retryWrites=true&w=majority")
-print(client)
+def get_db():
+    # Connection to MongoDB 
+    client = MongoClient("mongodb+srv://web340_admin:webstudent999@bellevueuniversity.bj68fz9.mongodb.net/?retryWrites=true&w=majority")
+    print(client)  # Print the client object
+    # Variable to access web335DB
+    db = client['whatABook']
+    return db
 
-# Adding a blank line to improve readability.
-print()
-
-# variable to access web335DB
-db = client['whatABook']
-
-# print all documents in books collection
-for book in db.books.find():
-  print(book)
-  
-# print list of books based on user's selected genre
-genre_Select = input("Please enter a genre from above:  ")
-if genre_Select == 'Horror' or genre_Select == 'Fantasy' or genre_Select == 'Fiction' or genre_Select == 'Romance' or genre_Select == 'Thriller' or genre_Select == 'Horror':
-    for book in db.books.find({'genre': genre_Select}, {'title': 1, 'genre': 1}):
+def print_books(db):
+    # Print all documents in books collection
+    print("\nList of all books:")
+    for book in db.books.find():
         print(book)
-else:
-    print("Please enter valid genre") 
 
-# Add a blank line to improve readability.
-print()
+def print_books_by_genre(db):
+    # Print list of books based on user's selected genre
+    genre_Select = input("\nPlease enter a genre from above:  ")
+    genres = ['Horror', 'Fantasy', 'Fiction', 'Romance', 'Thriller']
+    if genre_Select in genres:
+        print(f"\nList of books in genre {genre_Select}:")
+        for book in db.books.find({'genre': genre_Select}, {'title': 1, 'genre': 1}):
+            print(book)
+    else:
+        print("Please enter valid genre") 
 
-# Display list of books by genre
-select = input ('To view wishlist, please enter your customer ID. ')
+def print_wishlist(db):
+    # Display list of books by genre
+    select = input ('\nTo view wishlist, please enter your customer ID. ')
+    customer_ids = ['c1005', 'c1008', 'c1009', 'c1006', 'c1007']
+    if select in customer_ids:
+        print(f"\nWishlist for customer ID {select}:")
+        for wishlist in db.customers.find({'customerId': select}, {'wishlist': 1}):
+            print(wishlist)
+    else:
+        print("Please enter valid ID")
 
-# Add a blank line to improve readability.
-print()
+def main():
+    db = get_db()
+    print_books(db)
+    print_books_by_genre(db)
+    print_wishlist(db)
 
-if select == 'c1005' or select == 'c1008' or select == 'c1009' or select == 'c1006' or select == 'c1007':
-    for wishlist in db.customers.find({'customerId': select}, {'wishlist': 1}):
-        print(wishlist)
-else:
-    print("Please enter valid ID")
+if __name__ == "__main__":
+    main()
